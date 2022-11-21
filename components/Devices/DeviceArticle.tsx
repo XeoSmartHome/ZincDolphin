c8d56633081101c462ac0d643066ec7b2e1c0bd0
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2022-11-21 22:49:40 +0200

    device card update

--- components/Devices/DeviceArticle.tsx
+++ components/Devices/DeviceArticle.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import styles from "./DeviceArticle.module.scss";
+import ImageBackground from "../Common/ImageBackground";
 
 type DeviceArticleProps = {
     imagePosition: 'left' | 'right';
@@ -7,14 +8,20 @@ type DeviceArticleProps = {
 
 const DeviceArticle: FC<DeviceArticleProps> = ({imagePosition}) => {
     return (
-        <div className={styles.deviceArticle}>
+        <div className={`${styles.deviceArticle} ${imagePosition === "right" && styles.horizontalReverse}`}>
             <div className={styles.imageContainer}>
+                <div className={styles.imageWrapper}>
+                    <ImageBackground/>
+                </div>
             </div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>Device</h1>
+                <span className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <span className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+                    nunc non nunc aliquet, nec lacinia nisl aliquam. Sed euismod nunc non nunc aliquet, nec lacinia nisl
+                    aliquam.</span>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default DeviceArticle;
