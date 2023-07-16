40e61d62d3a2153751dfb71828754d0d1fae438f
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2023-07-17 02:51:19 +0300

    install more modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import styles from '../styles/Home.module.css';
-import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
 import Features from "../components/Features/Features";
 import DeviceArticle from "../components/Devices/DeviceArticle";
+import PageHeader from "../components/Common/PageHeader/PageHeader";
+import Head from "next/head";
 
 const HomePage = () => {
-    return (
-        <div className={styles.container}>
-            <Header/>
-            <Banner/>
-            <Features/>
-            <DeviceArticle imagePosition={"right"}/>
-            <DeviceArticle imagePosition={"left"}/>
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>XeoSmartHome</title>
+			</Head>
+			<PageHeader/>
+			<Banner/>
+			<Features/>
+			<DeviceArticle imagePosition={"right"}/>
+			<DeviceArticle imagePosition={"left"}/>
+		</div>
+	);
 };
 
 export default HomePage;
