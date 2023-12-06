f922485b451e2b44d8dbd17dbfcba6a9d7e88ff5
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2023-12-06 04:33:18 +0200

    ok

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,26 +1,29 @@
 import styles from "./Banner.module.scss";
 import ImageBackground from "../Common/ImageBackground";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Banner = () => {
-    return (
-        <div className={styles.banner}>
-            <ImageBackground/>
-            <div className={styles.content}>
-                <div className={styles.title}>
-                    High customisable IoT platform
-                </div>
-                <div className={styles.description}>
-                    Monitor and control your home at your fingertips.
-                </div>
-                <div className={styles.getStated}>
-                    <Button variant={"contained"}>
-                        Get Started
-                    </Button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.banner}>
+			<ImageBackground/>
+			<div className={styles.content}>
+				<Typography marginBottom={'1rem'} fontWeight={'bold'} fontSize={'1.8rem'}>
+					High customisable IoT platform
+				</Typography>
+				<Typography fontSize={'1.2rem'}>
+					Revolutionize your connected living – our customizable IoT platform lets you fine-tune your smart
+					home, offering personalized control over devices for a truly tailored and effortless lifestyle.
+				</Typography>
+				<Box marginTop={'2rem'}>
+					<Button variant={"contained"}>
+						Get Started
+					</Button>
+				</Box>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
