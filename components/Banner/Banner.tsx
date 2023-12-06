dfb66124bace9039f9380ca826250d89ecd30020
XeoSmartHome <xeosmarthome@gmail.com>
2023-12-06 20:20:45 +0200

    commit 2

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -3,27 +3,30 @@ import ImageBackground from "../Common/ImageBackground";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Link from "next/link";
 
 const Banner = () => {
-	return (
-		<div className={styles.banner}>
-			<ImageBackground/>
-			<div className={styles.content}>
-				<Typography marginBottom={'1rem'} fontWeight={'bold'} fontSize={'1.8rem'}>
-					High customisable IoT platform
-				</Typography>
-				<Typography fontSize={'1.2rem'}>
-					Revolutionize your connected living – our customizable IoT platform lets you fine-tune your smart
-					home, offering personalized control over devices for a truly tailored and effortless lifestyle.
-				</Typography>
-				<Box marginTop={'2rem'}>
-					<Button variant={"contained"}>
-						Get Started
-					</Button>
-				</Box>
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.banner}>
+            <ImageBackground/>
+            <div className={styles.content}>
+                <Typography marginBottom={'1rem'} fontWeight={'bold'} fontSize={'1.8rem'}>
+                    High customisable IoT platform
+                </Typography>
+                <Typography fontSize={'1.2rem'} fontWeight={'bold'}>
+                    Revolutionize your connected living – our customizable IoT platform lets you fine-tune your smart
+                    home, offering personalized control over devices for a truly tailored and effortless lifestyle.
+                </Typography>
+                <Box marginTop={'2rem'}>
+                    <Link href={'https://play.google.com/store/search?q=xeosmarthome&c=apps'} target={'_blank'}>
+                        <Button variant={"contained"}>
+                            Get started
+                        </Button>
+                    </Link>
+                </Box>
+            </div>
+        </div>
+    );
 };
 
 export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import Features from "../components/Features/Features";
 import DeviceArticle from "../components/Devices/DeviceArticle";
 import PageHeader from "../components/Common/PageHeader/PageHeader";
 import Head from "next/head";
+import {Meta} from "next/dist/lib/metadata/generate/meta";
 
 const HomePage = () => {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>XeoSmartHome</title>
+				<Meta content={"height=device-height, width=device-width, initial-scale=1.0"} name={"viewport"}/>
 			</Head>
 			<PageHeader/>
 			<Banner/>
