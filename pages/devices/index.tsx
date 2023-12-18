efedb5d1ad3f61cc4f8ef28fa5e049e0586d6ee6
XeoSmartHome <xeosmarthome@gmail.com>
2023-12-18 17:49:57 +0200

    commit 3

--- pages/devices/index.tsx
+++ pages/devices/index.tsx
@@ -1,6 +1,9 @@
+import PageHeader from "../../components/Common/PageHeader/PageHeader";
+
 const DevicesPage = () => {
     return (
         <div>
+            <PageHeader/>
             <h1>Devices 123</h1>
         </div>
     );
