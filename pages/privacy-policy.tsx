5aad1c4926fae4cb0e32431b8b7c8bc3facad8ad
XeoSmartHome <xeosmarthome@gmail.com>
2024-03-07 16:03:02 +0200

    Privacy Policy for XeoSmartHome

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,132 @@
+import {FC} from "react";
+import Box from "@mui/material/Box";
+
+const PrivacyPolicyPage: FC = () => {
+	return (
+		<Box display={'flex'} justifyContent={'center'}>
+			<Box paddingY={'5rem'} width={'50%'} minWidth={'30rem'}>
+				<h1>
+					Privacy Policy for XeoSmartHome
+				</h1>
+
+				<h2>
+					Effective Date: 7 March 2024
+				</h2>
+
+				<p>
+					Welcome to XeoSmartHome ("XeoSmartHome", "we", "us", or "our"). This Privacy Policy describes how we
+					collect, use, and disclose personal information when you use our website located at
+					https://xeosmarthome.com (the "Website") and our mobile application named XeoApp (the "App"),
+					collectively referred to as the "Platform". By accessing or using the Platform, you agree to the
+					terms of this Privacy Policy.
+				</p>
+
+				<h3>
+					1. Information We Collect
+				</h3>
+
+				<p>
+					a. Information You Provide: We collect personal information that you voluntarily provide to us when
+					you create an account, register for services, or contact us for support. This information may
+					include your name, email address, phone number, and password.
+				</p>
+
+				<p>
+					b. Usage Data: We may also collect information about how you interact with the Platform, such as
+					your device type, IP address, browser type, pages visited, and actions taken.
+				</p>
+
+				<h3>
+					2. Use of Information
+				</h3>
+
+				<p>
+					a. Purpose: We use the information we collect to provide, maintain, and improve the Platform, as
+					well as to communicate with you about your account and updates to our services.
+				</p>
+
+				<p>
+					b. Analytics: We may use third-party analytics tools, such as Firebase Analytics, to analyze usage
+					patterns and trends on the Platform.
+				</p>
+
+				<h3>
+					3. Disclosure of Information
+				</h3>
+
+				<p>
+					a. Service Providers: We may disclose personal information to third-party service providers who
+					assist us in providing the Platform, such as hosting providers, payment processors, and analytics
+					providers.
+				</p>
+
+				<p>
+					b. Legal Compliance: We may disclose personal information if required to do so by law or in response
+					to valid requests from law enforcement or other governmental authorities.
+				</p>
+
+				<h3>
+					4. Data Security
+				</h3>
+
+				<p>
+					a. Security Measures: We take reasonable measures to protect the security of your personal
+					information and prevent unauthorized access, use, or disclosure.
+				</p>
+
+				<p>
+					b. Data Retention: We retain personal information for as long as necessary to fulfill the purposes
+					outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.
+				</p>
+
+				<h3>
+					5. Your Choices
+				</h3>
+				<p>
+					a. Account Information: You may update or correct your account information at any time by logging
+					into your account settings.
+				</p>
+
+				<h3>
+					6. Children's Privacy
+				</h3>
+
+				<p>
+					a. Age Requirement: The Platform is not intended for children under the age of 13, and we do not
+					knowingly collect personal information from children under this age.
+				</p>
+
+				<h3>
+					7. Changes to this Privacy Policy
+				</h3>
+
+				<p>
+					a. Updates: We may update this Privacy Policy from time to time by posting the revised version on
+					the
+					Website or through the App. Your continued use of the Platform after the posting of the updated
+					Privacy
+					Policy constitutes your acceptance of the changes.
+				</p>
+
+				<h3>
+					8. Contact Us
+				</h3>
+
+				<p>
+					a. Inquiries: If you have any questions or concerns about this Privacy Policy, please contact us at
+					xeosmarthome@gmail.com.
+				</p>
+
+				<h4>
+					By using the Platform, you acknowledge that you have read, understood, and agree to the terms of
+					this
+					Privacy Policy. If you do not agree to this Privacy Policy, you may not access or use the
+					Platform.
+				</h4>
+
+			</Box>
+		</Box>
+	)
+}
+
+export default PrivacyPolicyPage;
